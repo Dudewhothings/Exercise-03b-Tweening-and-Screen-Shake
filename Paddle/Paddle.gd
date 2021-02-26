0df0648f1e2d10dc2625785a08a698d6e6891688
Dudewhothings <rabbidblaster@gmail.com>
2021-02-26 14:31:37 -0500

    Update

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -48,11 +48,17 @@ func _physics_process(_delta):
 	var t = clamp(d, d, speed)							# how much to move the paddle this cycle (maximum of speed)
 	var s = sign(target - position.x)					# which direction to move
 	position.x += s*t
-
 	if HUD.paddle_stretch:
-		pass
+		var w = 1 + (distort.x * p)
+		var h = 1 - (1/distort.y * p)
+		change_size(w,h)
+		color.s = color_s * (1-p)
+		update_color()
 
 
 func start_paddle():
 	if HUD.paddle_appear:
-		pass
+		var target_pos = position
+		position.y = -100
+		$Tween.interpolate_property(self, "position", position, target_pos, fall_duration, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+		$Tween.start()
